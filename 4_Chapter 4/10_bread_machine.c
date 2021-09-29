b6b8a927e935fd779bb963c6f4ea13b991b73166
Robby Pambudi <robby.pambudi10@gmail.com>
2021-09-30 00:09:42 +0700

    Penambahan Soal No 10 11 Not Final

---
+++ 4_Chapter 4/10_bread_machine.c
@@ -0,0 +1,18 @@
+/*
+Write a program to control a bread machine. 
+Allow the user to input the type of bread as W for White and S for Sweet.
+Ask the user if the loaf size is double and if the baking is manual. 
+The following table details the time chart for the machine for each bread type. 
+Display a statement for each step. If the loaf size is double, 
+increase the baking time by 50%. If baking is manual, stop after the loaf-shaping 
+cycle and instruct the user to remove the dough for manual baking. Use functions to 
+display instructions to the user and to compute the baking time.
+*/
+
+#include <stdio.h>
+
+int main (){
+    char T;                 // For Type 
+    scanf("%c", &T);
+    
+}
---
+++ 4_Chapter 4/11_boiling_points.c
@@ -0,0 +1,8 @@
+/*
+The table below shows the normal boiling points of several substances. 
+Write a program that prompts the user for the observed boiling point 
+of a substance in °C and identifies the substance if the observed 
+boiling point is within 5% of the expected boiling point. If the 
+data input is more than 5% higher or lower than any of the boiling 
+points in the table, the program should output the message Substance unknown.
+*/
